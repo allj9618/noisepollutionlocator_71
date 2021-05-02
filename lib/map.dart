d8ddfcb2a0e02cc0731549ae6e7065df90447cdc
Christoffer <ccohman@gmail.com>
2021-05-02 23:58:31 +0200

    Created new/rewrote  Map.class using flutter_map, added google maps as base layer.

--- lib/map.dart
+++ lib/map.dart
@@ -1,63 +1,39 @@
-import 'package:platform_maps_flutter/platform_maps_flutter.dart';
 import 'package:flutter/material.dart';
-
-
-
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class Map extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PlatformMap(
-        initialCameraPosition: CameraPosition(
-          target: const LatLng(59.3350, 18.0406),
-          zoom: 16.0,
-        ),
-        markers: Set<Marker>.of(
-          [
-            Marker(
-              markerId: MarkerId('marker_1'),
-              position: LatLng(59.3350, 18.0406),
-              consumeTapEvents: true,
-              infoWindow: InfoWindow(
-                title: 'PO Hallmansgatan 10',
-                snippet: "Noise pollution level: High",
-              ),
-              onTap: () {
-                print("Marker tapped");
-              },
+        body: FlutterMap(
+          options: MapOptions(
+
+
+            // Setting coordinates to Stockholm
+            center: LatLng(59.3294, 18.0686),
+            zoom: 9.0,
+          ),
+
+          layers: [
+            TileLayerOptions(
+              urlTemplate:
+              'https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+              subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+
+
+              tileProvider: CachedNetworkTileProvider(),
             ),
+
+
+
           ],
         ),
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        onTap: (location) => print('onTap: $location'),
-        onCameraMove: (cameraUpdate) => print('onCameraMove: $cameraUpdate'),
-        compassEnabled: true,
-        onMapCreated: (controller) {
-          Future.delayed(Duration(seconds: 2)).then(
-                (_) {
-              controller.animateCamera(
-                CameraUpdate.newCameraPosition(
-                  const CameraPosition(
-                    bearing: 270.0,
-                    target: LatLng(59.3350, 18.0406),
-                    tilt: 30.0,
-                    zoom: 18,
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
     );
   }
+
+
 }
-
-
-
-
 
 /* Backup before changes // CÖ
 
