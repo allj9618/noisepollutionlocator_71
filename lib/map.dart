6914d14a86f362691dec24af485785ec07756a63
Alfred Ljung <allj9618@student.su.se>
2021-05-17 15:03:53 +0200

    Changed Center in Map class to Stack. Added Container for image with Map key. Added Map key showing decibel values and respective colors.

--- lib/map.dart
+++ lib/map.dart
@@ -13,29 +13,30 @@ class _Map extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center (
-        child: FlutterMap(
-            options: MapOptions(
-              // Setting coordinates to Stockholm
-              center: LatLng(59.3294, 18.0686),
-              zoom: 14.0,
-            ),
-            layers: [
-              // Google maps layer
-              TileLayerOptions(
-                  urlTemplate: 'https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-                  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-                  backgroundColor: Colors.transparent),
+      body: Stack (
+        children: [
+        FlutterMap(
+          options: MapOptions(
+            // Setting coordinates to Stockholm
+            center: LatLng(59.3294, 18.0686),
+            zoom: 14.0,
+          ),
+          layers: [
+            // Google maps layer
+            TileLayerOptions(
+                urlTemplate: 'https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                backgroundColor: Colors.transparent),
 
-              // Bullerdata layer from Stockholms stad
-              TileLayerOptions(
-                  wmsOptions: WMSTileLayerOptions(
-                      baseUrl: "http://kartor.miljo.stockholm.se/geoserver/wms?",
-                      layers: ["mfraster:bullerkartan-2012-allakallor"],
-                      transparent: false,
-                      format: "image/png"),
-                  opacity: noiseLayerIsOn ? 0.4 : 0.0,
-                  backgroundColor: Colors.transparent),
+            // Bullerdata layer from Stockholms stad
+            TileLayerOptions(
+                wmsOptions: WMSTileLayerOptions(
+                    baseUrl: "http://kartor.miljo.stockholm.se/geoserver/wms?",
+                    layers: ["mfraster:bullerkartan-2012-allakallor"],
+                    transparent: false,
+                    format: "image/png"),
+                opacity: noiseLayerIsOn ? 0.4 : 0.0,
+                backgroundColor: Colors.transparent),
 
 /*
 
@@ -54,8 +55,15 @@ class _Map extends State<Map> {
 
           ),
     */
-            ],
-      )
+          ],
+        ),
+          Container(
+            child: Image.asset('assets/dbkey.png'),
+            alignment: Alignment.topRight,
+            width: 500,
+            height: 200,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text("Noise Layer"),
