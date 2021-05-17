bdccc3d5401a7f5c0b49ad14934cdb0cdd424d05
Alfred Ljung <allj9618@student.su.se>
2021-05-17 12:51:14 +0200

    Changed Map class to Stateful widget. Added SwitchListTile for switching on and off noisepollution layer on the map. Done by changing opacity of layer when clicking the switch.

--- lib/map.dart
+++ lib/map.dart
@@ -2,32 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
+  @override
+  _Map createState() => _Map();
+}
+
+class _Map extends State<Map> {
+  double noiseLayerOpacity = 0.4;
+  bool noiseLayerIsOn = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-          // Setting coordinates to Stockholm
-          center: LatLng(59.3294, 18.0686),
-          zoom: 14.0,
-        ),
-        layers: [
-          // Google maps layer
-          TileLayerOptions(
-              urlTemplate: 'https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-              subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-              backgroundColor: Colors.transparent),
+      body: Center (
+        child: FlutterMap(
+            options: MapOptions(
+              // Setting coordinates to Stockholm
+              center: LatLng(59.3294, 18.0686),
+              zoom: 14.0,
+            ),
+            layers: [
+              // Google maps layer
+              TileLayerOptions(
+                  urlTemplate: 'https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                  backgroundColor: Colors.transparent),
 
-          // Bullerdata layer from Stockholms stad
-          TileLayerOptions(
-              wmsOptions: WMSTileLayerOptions(
-                  baseUrl: "http://kartor.miljo.stockholm.se/geoserver/wms?",
-                  layers: ["mfraster:bullerkartan-2012-allakallor"],
-                  transparent: false,
-                  format: "image/png"),
-              opacity: 0.4,
-              backgroundColor: Colors.transparent),
+              // Bullerdata layer from Stockholms stad
+              TileLayerOptions(
+                  wmsOptions: WMSTileLayerOptions(
+                      baseUrl: "http://kartor.miljo.stockholm.se/geoserver/wms?",
+                      layers: ["mfraster:bullerkartan-2012-allakallor"],
+                      transparent: false,
+                      format: "image/png"),
+                  opacity: noiseLayerIsOn ? 0.4 : 0.0,
+                  backgroundColor: Colors.transparent),
 
 /*
 
@@ -46,8 +55,20 @@ class Map extends StatelessWidget {
 
           ),
     */
-        ],
+            ],
+      )
       ),
+      bottomSheet:
+      SwitchListTile(
+        title: Text("Show/Hide Noise Pollution Layer"),
+        value: noiseLayerIsOn,
+        onChanged: (value) {
+          setState(() {
+            noiseLayerIsOn = value;
+          });
+        },
+      ),
+      backgroundColor: Colors.transparent,
     );
   }
 }
