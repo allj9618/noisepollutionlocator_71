f7a4daad7cac8ccdf3c13d4ba19544c9c57f78fb
Alfred Ljung <allj9618@student.su.se>
2021-05-24 13:31:23 +0200

    Added Firebase to project. Added google-services.json file. Added Google Sign In getting username, email and profile image.

---
+++ lib/google_sign_in.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn;
+GoogleSignInAccount _currentUser;
+
+class SignInGoogle extends StatefulWidget {
+  @override
+  State createState() => _SignInGoogle();
+}
+
+class _SignInGoogle extends State<SignInGoogle> {
+
+  Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
+  @override
+  initState() {
+    super.initState();
+
+    _googleSignIn = GoogleSignIn(
+      scopes: <String>[
+        'email',
+        'profile',
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ],
+    );
+
+    _googleSignIn.onCurrentUserChanged.listen((user) {
+      setState(() {
+        _currentUser = user;
+      });
+    });
+  }
+
+  Widget getLoginWidget() {
+    GoogleSignInAccount user = _currentUser;
+    if (user != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GoogleUserCircleAvatar(
+            identity: user,
+          ),
+          Text(user.displayName ?? ''),
+          Text(user.email),
+          ElevatedButton(
+              onPressed: () async {
+                _googleSignIn.signOut();
+                _handleSignOut();
+              },
+              child: Text("Sign Out"))
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () async {
+                try {
+                  await _googleSignIn.signIn();
+                } on Exception catch (e) {
+                  print(e);
+                }
+              },
+              child: Text("Google Sign In")),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Google Sign In'),
+        ),
+        body: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          child: getLoginWidget(),
+        ));
+  }
+}
