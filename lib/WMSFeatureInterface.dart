9ac620ae1fb7b10c7e05968f2323c96da7f703b9
Christoffer <ccohman@gmail.com>
2021-05-28 13:03:33 +0200

    Added calculations for Bbox offsets.

--- lib/WMSFeatureInterface.dart
+++ lib/WMSFeatureInterface.dart
@@ -1,7 +1,17 @@
 ///  Class for fetching features from a WMS server.
 ///  @author Christoffer Öhman
+  import "package:latlong/latlong.dart" as LatLng;
 
 class WMSFeatureInterface {
 
+  Future<Map> getFeature(LatLng.LatLng coordinates) async {
 
+    final double bBoxLimiter = 0.0001;
+    String bBox = (coordinates.latitude - bBoxLimiter).toString() + ',' +
+        (coordinates.longitude - bBoxLimiter).toString() + ',' +
+        (coordinates.latitude + bBoxLimiter).toString() + ',' +
+        (coordinates.longitude + bBoxLimiter).toString();
+
+    return null;
+  } // getFeature
 } // FeatureInterface
