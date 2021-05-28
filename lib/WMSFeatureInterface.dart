e10698c537c7cbd4bc4d65efe72697e81b5ea815
Christoffer <ccohman@gmail.com>
2021-05-28 19:25:40 +0200

    added code so that WMSFeatureInterface now returns the dB value as an int.

--- lib/WMSFeatureInterface.dart
+++ lib/WMSFeatureInterface.dart
@@ -11,7 +11,7 @@ class WMSFeatureInterface {
 
 
 
-  Future<Map> getFeature(LatLng.LatLng coordinates) async {
+  Future<int> getFeature(LatLng.LatLng coordinates) async {
     final double bBoxLimiter = 0.0001;
     final String featureURL =
         "http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&";
@@ -42,19 +42,39 @@ class WMSFeatureInterface {
             "&HEIGHT=101"
             "&BBOX=" + bBox;
 
-    final response = await http.get(Uri.parse(featureURL + parameters));
-    //  final response = await http.get(Uri.parse("http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetFeatureInfo&FORMAT=application/json&TRANSPARENT=true&QUERY_LAYERS=mfraster%3Abullerkartan-2012-allakallor&LAYERS=mfraster%3Abullerkartan-2012-allakallor&exceptions=application/vnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=50000&X=50&Y=50&SRS=EPSG:4326&STYLES=&WIDTH=101&HEIGHT=101&BBOX=18.076506,59.309229,18.084231,59.311414"));
+    //final response = await http.get(Uri.parse(featureURL + parameters));
+     final response = await http.get(Uri.parse("http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetFeatureInfo&FORMAT=application/json&TRANSPARENT=true&QUERY_LAYERS=mfraster%3Abullerkartan-2012-allakallor&LAYERS=mfraster%3Abullerkartan-2012-allakallor&exceptions=application/vnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=50000&X=50&Y=50&SRS=EPSG:4326&STYLES=&WIDTH=101&HEIGHT=101&BBOX=18.076506,59.309229,18.084231,59.311414"));
     if (response.statusCode != 200) {
       throw Exception('Failed to fetch WMS feature');
     }
 
-    print (response.body);
+   // print (response.body);
 // parse the result from GeoJson to dynamic Map.
     Map<String, dynamic> result = jsonDecode(response.body);
 
-    print (result);// testing
 
-    return result;
+
+
+    // testing api and dB readings below.
+    //----------
+
+
+    print ("result printed from interface: $result)");
+
+      var features = result['features'];
+      Map <String, dynamic> featuresMap = new Map <String, dynamic>.from(features[0]);
+      var properties = featuresMap['properties'];
+    Map <String, dynamic> propertiesMap =new Map <String, dynamic>.from(properties);
+
+    int dBValue = propertiesMap['GRAY_INDEX'];
+
+      String testString = dBValue.toString();
+      print(" result from db api test: $testString");
+
+
+
+
+    return dBValue;
 
   } // getFeature
 } // FeatureInterface
