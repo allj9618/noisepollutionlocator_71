bea071a75a1ded1b9a2eb2c089ed87003e0d1ae4
Christoffer <ccohman@gmail.com>
2021-05-28 20:01:04 +0200

    Changed places for longitude and latitude, (different for different versions of GeoServer.
    Added some test printers to api interface.

--- lib/WMSFeatureInterface.dart
+++ lib/WMSFeatureInterface.dart
@@ -1,36 +1,35 @@
-
 ///  Class for fetching features from a WMS server.
 ///  @author Christoffer Öhman
-import 'package:http/http.dart' as http;
-import "package:latlong/latlong.dart" as LatLng;
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import "package:latlong/latlong.dart" as LatLng;
 
 class WMSFeatureInterface {
-
-
-
-
   Future<int> getFeature(LatLng.LatLng coordinates) async {
     final double bBoxLimiter = 0.0001;
     final String featureURL =
         "http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&";
     final String wmsLayer = "mfraster%3Abullerkartan-2012-allakallor";
-    String bBox = (coordinates.latitude - bBoxLimiter).toString() +
+
+    // longitude and latitude is entered in different order for different WMS versions.
+    String bBox = (coordinates.longitude - bBoxLimiter).toString() +
         ',' +
-        (coordinates.longitude - bBoxLimiter).toString() +
+        (coordinates.latitude - bBoxLimiter).toString() +
         ',' +
-        (coordinates.latitude + bBoxLimiter).toString() +
+        (coordinates.longitude + bBoxLimiter).toString() +
         ',' +
-        (coordinates.longitude + bBoxLimiter).toString();
+        (coordinates.latitude + bBoxLimiter).toString();
 
     String parameters = "SERVICE=WMS"
-        "&VERSION=1.1.0"
-        "&REQUEST=GetFeatureInfo"
-        "&FORMAT=application/json"
-        "&TRANSPARENT=true"
-        "&QUERY_LAYERS=" + wmsLayer +
-        "&LAYERS=" + wmsLayer +
+            "&VERSION=1.1.0"
+            "&REQUEST=GetFeatureInfo"
+            "&FORMAT=application/json"
+            "&TRANSPARENT=true"
+            "&QUERY_LAYERS=" +
+        wmsLayer +
+        "&LAYERS=" +
+        wmsLayer +
         "&exceptions=application/vnd.ogc.se_inimage"
             "&INFO_FORMAT=application/json"
             "&FEATURE_COUNT=50"
@@ -40,41 +39,33 @@ class WMSFeatureInterface {
             "&STYLES="
             "&WIDTH=101"
             "&HEIGHT=101"
-            "&BBOX=" + bBox;
+            "&BBOX=" +
+        bBox;
 
-    //final response = await http.get(Uri.parse(featureURL + parameters));
-     final response = await http.get(Uri.parse("http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetFeatureInfo&FORMAT=application/json&TRANSPARENT=true&QUERY_LAYERS=mfraster%3Abullerkartan-2012-allakallor&LAYERS=mfraster%3Abullerkartan-2012-allakallor&exceptions=application/vnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=50000&X=50&Y=50&SRS=EPSG:4326&STYLES=&WIDTH=101&HEIGHT=101&BBOX=18.076506,59.309229,18.084231,59.311414"));
+    final response = await http.get(Uri.parse(featureURL + parameters));
+   // final response = await http.get(Uri.parse("http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetFeatureInfo&FORMAT=application/json&TRANSPARENT=true&QUERY_LAYERS=mfraster%3Abullerkartan-2012-allakallor&LAYERS=mfraster%3Abullerkartan-2012-allakallor&exceptions=application/vnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=50000&X=50&Y=50&SRS=EPSG:4326&STYLES=&WIDTH=101&HEIGHT=101&BBOX=18.076506,59.309229,18.084231,59.311414"));
     if (response.statusCode != 200) {
       throw Exception('Failed to fetch WMS feature');
     }
 
-   // print (response.body);
+    // print (response.body);
 // parse the result from GeoJson to dynamic Map.
     Map<String, dynamic> result = jsonDecode(response.body);
 
-
-
-
-    // testing api and dB readings below.
-    //----------
-
-
-    print ("result printed from interface: $result)");
-
-      var features = result['features'];
-      Map <String, dynamic> featuresMap = new Map <String, dynamic>.from(features[0]);
-      var properties = featuresMap['properties'];
-    Map <String, dynamic> propertiesMap =new Map <String, dynamic>.from(properties);
+    var features = result['features'];
+    Map<String, dynamic> featuresMap =
+        new Map<String, dynamic>.from(features[0]);
+    var properties = featuresMap['properties'];
+    Map<String, dynamic> propertiesMap =
+        new Map<String, dynamic>.from(properties);
 
     int dBValue = propertiesMap['GRAY_INDEX'];
 
-      String testString = dBValue.toString();
-      print(" result from db api test: $testString");
-
-
-
+    // used for testing
+    print("result printed from interface: $result)"); // used for testing
+    String testString = dBValue.toString();
+    print(" result from db api test: $testString");
 
     return dBValue;
-
   } // getFeature
 } // FeatureInterface
