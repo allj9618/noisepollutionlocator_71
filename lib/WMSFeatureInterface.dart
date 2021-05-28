b7935faa6fa10268f05fa32ca726f0989b9e8d13
Christoffer <ccohman@gmail.com>
2021-05-28 13:13:03 +0200

    added parameters and featureURL.

--- lib/WMSFeatureInterface.dart
+++ lib/WMSFeatureInterface.dart
@@ -10,6 +10,7 @@ class WMSFeatureInterface {
     final double bBoxLimiter = 0.0001;
     final String featureURL =
         "http://kartor.miljo.stockholm.se/geoserver/mfraster/wms?SERVICE=WMS&";
+    final String wmsLayer = "mfraster%3Abullerkartan-2012-allakallor";
     String bBox = (coordinates.latitude - bBoxLimiter).toString() +
         ',' +
         (coordinates.longitude - bBoxLimiter).toString() +
@@ -17,6 +18,23 @@ class WMSFeatureInterface {
         (coordinates.latitude + bBoxLimiter).toString() +
         ',' +
         (coordinates.longitude + bBoxLimiter).toString();
+
+    String parameters = "SERVICE=WMS&VERSION=1.1.0"
+        "&REQUEST=GetFeatureInfo"
+        "&FORMAT=application/json"
+        "&TRANSPARENT=true"
+        "&QUERY_LAYERS=" + wmsLayer +
+        "&LAYERS=" + wmsLayer +
+        "&exceptions=application/vnd.ogc.se_inimage"
+            "&INFO_FORMAT=application/json"
+            "&FEATURE_COUNT=50"
+            "&X=50"
+            "&Y=50"
+            "&SRS=EPSG:4326"
+            "&STYLES="
+            "&WIDTH=101"
+            "&HEIGHT=101"
+            "&BBOX=" + bBox;
 
 
     final response = await http.get(Uri.parse(featureURL));
@@ -26,6 +44,5 @@ class WMSFeatureInterface {
     }
 
 
-    return null;
   } // getFeature
 } // FeatureInterface
