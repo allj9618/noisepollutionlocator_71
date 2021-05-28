e62228962928cfec2613da51cec0a07426258c79
Christoffer <ccohman@gmail.com>
2021-05-28 13:32:01 +0200

    Created UnitTest for WMSFeatureInterFace

---
+++ test/WMSFeatureInterFaceTest.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:noisepollutionlocator_71/WMSFeatureInterface.dart';
+import "package:latlong/latlong.dart" as LatLng;
+
+
+void main() {
+
+  WMSFeatureInterface wms = new WMSFeatureInterface();
+  LatLng.LatLng coordinates = new LatLng.LatLng(18.076506,59.309229);
+
+  print (wms.getFeature(coordinates));
+
+}
